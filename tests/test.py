c78704dcc7b36e6af9622027be8b4976f5e0f241
fcakyon <fcakyon@gmail.com>
2020-05-26 01:12:30 +0300

    update tests

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ from confplot import (
 
 
 class TestPrettyConfMat(unittest.TestCase):
-    def test_data_class(self):
+    def test_from_data(self):
         """ test function y_true (actual values) and y_pred (predictions) """
         #data
         y_true = np.array([1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5, 1,2,3,4,5])
@@ -22,31 +22,38 @@ class TestPrettyConfMat(unittest.TestCase):
         columns = None
         annot = True
         cmap = 'Oranges'
-        fmt = '.2f'
         lw = 0.5
         cbar = False
-        show_null_values = 2
+        show_null_values = 1
         pred_val_axis = 'y'
         fz = 12
-        figsize = [9, 9]
-        if(len(y_true) > 10):
+        figsize = [7, 7]
+        if(len(np.unique(y_true)) > 10):
             fz = 9; figsize = [14, 14]
         plot_confusion_matrix_from_data(y_true, y_pred, columns,
-        annot, cmap, fmt, fz, lw, cbar, figsize, show_null_values, pred_val_axis)
+        annot, cmap, fz, lw, cbar, figsize, show_null_values, pred_val_axis)
 
-    def test_cm(self):
+    def test_from_matrix(self):
         #test function with confusion matrix done
-        array = np.array( [[13,  0,  1,  0,  2,  0],
-                        [ 0, 50,  2,  0, 10,  0],
-                        [ 0, 13, 16,  0,  0,  3],
-                        [ 0,  0,  0, 13,  1,  0],
-                        [ 0, 40,  0,  1, 15,  0],
-                        [ 0,  0,  0,  0,  0, 20]])
+        array = np.array(
+            [[13,  0,  1,  0,  2,  0],
+             [ 0, 50,  2,  0, 10,  0],
+             [ 0, 13, 16,  0,  0,  3],
+             [ 0,  0,  0, 13,  1,  0],
+             [ 0, 40,  0,  1, 15,  0],
+             [ 0,  0,  0,  0,  0, 20]]
+        )
         #get pandas dataframe
         df_cm = DataFrame(array, index=range(1, 7), columns=range(1, 7))
         #colormap: see this and choose your more dear
         cmap = 'PuRd'
-        plot_confusion_matrix_from_matrix(df_cm, cmap=cmap)
+        show_null_values = 0
+        figsize = [7, 7]
+        if(len(array) > 10):
+            fz = 9; figsize = [14, 14]
+        plot_confusion_matrix_from_matrix(
+            df_cm, cmap=cmap, figsize=figsize, show_null_values=show_null_values
+        )
 
 
 if __name__ == '__main__':
